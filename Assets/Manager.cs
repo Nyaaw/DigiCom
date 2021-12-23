6f7258d4d4c3e7573c419fc81cae2054306302a3
LoicMugny <75372873+aevangelistDev@users.noreply.github.com>
2021-12-23 15:58:06 +0100

    feat: Implementing code to arrange the elements on the table
    Added some code that manages the position of the elements on the table and do another row if the row exceeds 5 elements

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -7,28 +7,51 @@ public class Manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        const float SCALEMULTIPLIER = 3.5f;
+        const float DIRECTORIESHEIGHT = 1.3f;
+        const float EDGEOFTABLE = 0.4325f;
+        const float GAPBETWEENELEMENTS = 0.2171f;
         List<string> directoryList = new List<string>(System.IO.Directory.EnumerateDirectories("C:/Users/Admin/Documents/Dolphin Emulator"));
         int i = 0;
         int sizeOfTable = 8;
+        float x;
+        float z;
+        Vector3 scale;
+        int xInTable;
+        int yInTable;
         foreach (string directory in directoryList)
         {
             i+= sizeOfTable;
             GameObject table = GameObject.CreatePrimitive(PrimitiveType.Cube);
             table.transform.localScale = new Vector3(sizeOfTable, 1, sizeOfTable);
-            table.transform.position = new Vector3(0, 0, i++);
-            Debug.Log(directory.ToString());
+            table.transform.position = new Vector3(0, 1, i++);
             List<string> subDirectoriesList = new List<string>(System.IO.Directory.EnumerateDirectories(directory.ToString()));
-            foreach (string subDir in subDirectoriesList)
+            List<string> subFilesList = new List<string>(System.IO.Directory.EnumerateFiles(directory.ToString()));
+            List<string> subElementsList = new List<string>(subDirectoriesList);
+            subElementsList.AddRange(subFilesList);
+
+            xInTable = 0;
+            yInTable = 0;
+            foreach (string subElement in subElementsList)
             {               
                 GameObject subCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 subCube.transform.SetParent(table.transform);
 
-                Vector3 scale = subCube.transform.localScale;
-                float x = scale.x;
-                float z = scale.z;
+                scale = subCube.transform.localScale;
+                x = scale.x;
+                z = scale.z;
 
-                i += 2;
-                subCube.transform.position = new Vector3(0, 1, i);
+                if ((-SCALEMULTIPLIER * z) + xInTable * GAPBETWEENELEMENTS >= EDGEOFTABLE)
+                {
+                    xInTable = 0;
+                    yInTable++;
+                }
+
+                x = (-SCALEMULTIPLIER * x) + yInTable * GAPBETWEENELEMENTS;
+                z = (-SCALEMULTIPLIER * z) + xInTable * GAPBETWEENELEMENTS;
+
+                subCube.transform.localPosition = new Vector3(x, DIRECTORIESHEIGHT, z);
+                xInTable++;
             }
         }
     }
