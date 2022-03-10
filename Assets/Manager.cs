3a46e10fbf676420b7f52d23e6d8ac8e56a82e16
LoicMugny <75372873+aevangelistDev@users.noreply.github.com>
2022-03-10 16:02:32 +0100

    feat: Refactored the whole code using a function and added text management
    Refactored the whole generation of shapes code to use it from a function to avoid code duplication, and managed to clone and change the text of a TextMeshPro object

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -5,89 +5,99 @@ using System.Text;
 using System.IO;
 using TMPro;
 
+
+
 public class Manager : MonoBehaviour
 {
     [SerializeField]
-    private List<string> subFilesList;
     // Start is called before the first frame update
+
+    const float SCALE_MULTIPLIER = 3.5f;
+    const float DIRECTORIES_HEIGHT = 1.3f;
+    const float EDGE_OF_TABLE = 0.4325f;
+    const float GAP_BETWEEN_ELEMENTS = 0.2171f;
+    const int GAP_BETWEEN_TABLES = 4;
+    int i = 0;
+    int sizeOfTable = 8;
+    float x;
+    float z;
+    Vector3 scale;
+    int xInTable;
+    int yInTable;
+    private static DirectoryInfo sourceDirectory = new DirectoryInfo("C:\\Users\\Admin\\Documents\\Dolphin Emulator");
+    IEnumerable<DirectoryInfo> directoryList = sourceDirectory.EnumerateDirectories();
+    void generateSubElement(object typeOfElement, GameObject tableParam)
+    {
+        GameObject subElement;
+        if (typeOfElement is DirectoryInfo)
+        {
+            subElement = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        }
+        else if (typeOfElement is FileInfo)
+        {
+            subElement = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        }
+        else
+        {
+            return;
+        }
+        subElement.transform.SetParent(tableParam.transform);
+
+        scale = subElement.transform.localScale;
+        x = scale.x;
+        z = scale.z;
+
+        if ((-SCALE_MULTIPLIER * z) + xInTable * GAP_BETWEEN_ELEMENTS >= EDGE_OF_TABLE)
+        {
+            xInTable = 0;
+            yInTable++;
+        }
+
+        x = (-SCALE_MULTIPLIER * x) + yInTable * GAP_BETWEEN_ELEMENTS;
+        z = (-SCALE_MULTIPLIER * z) + xInTable * GAP_BETWEEN_ELEMENTS;
+
+        subElement.transform.localPosition = new Vector3(x, DIRECTORIES_HEIGHT, z);
+        xInTable++;
+    }
+
     void Start()
     {
-        const float SCALE_MULTIPLIER = 3.5f;
-        const float DIRECTORIES_HEIGHT = 1.3f;
-        const float EDGE_OF_TABLE = 0.4325f;
-        const float GAP_BETWEEN_ELEMENTS = 0.2171f;
-        const int GAP_BETWEEN_TABLES = 4;
-        int i = 0;
-        int sizeOfTable = 8;
-        float x;
-        float z;
-        Vector3 scale;
-        int xInTable;
-        int yInTable;
-        DirectoryInfo di = new DirectoryInfo("C:\\Users\\Admin\\Documents\\Dolphin Emulator");
-        IEnumerable<DirectoryInfo> directoryList = di.EnumerateDirectories();
+        TextMeshPro tModel = GameObject.FindObjectOfType<TextMeshPro>();
 
+        TextMeshPro tClone = Object.Instantiate(tModel);
+
+        
         foreach (DirectoryInfo directory in directoryList)
         {
-            i+= (sizeOfTable + GAP_BETWEEN_TABLES);
+            i += (sizeOfTable + GAP_BETWEEN_TABLES);
             GameObject table = GameObject.CreatePrimitive(PrimitiveType.Cube);
             table.transform.localScale = new Vector3(sizeOfTable, 1, sizeOfTable);
             table.transform.position = new Vector3(0, 1, i);
+            
+
             IEnumerable<DirectoryInfo> subDirectoriesList = directory.EnumerateDirectories();
-            IEnumerable<FileInfo> subFilesList = directory.EnumerateFiles();
 
             xInTable = 0;
             yInTable = 0;
+
             foreach (DirectoryInfo subDirectory in subDirectoriesList)
-            {               
-                GameObject subCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                subCube.transform.SetParent(table.transform);
-
-                scale = subCube.transform.localScale;
-                x = scale.x;
-                z = scale.z;
-
-                if ((-SCALE_MULTIPLIER * z) + xInTable * GAP_BETWEEN_ELEMENTS >= EDGE_OF_TABLE)
-                {
-                    xInTable = 0;
-                    yInTable++;
-                }
-
-                x = (-SCALE_MULTIPLIER * x) + yInTable * GAP_BETWEEN_ELEMENTS;
-                z = (-SCALE_MULTIPLIER * z) + xInTable * GAP_BETWEEN_ELEMENTS;
-
-                subCube.transform.localPosition = new Vector3(x, DIRECTORIES_HEIGHT, z);
-                xInTable++;
+            {
+                generateSubElement(subDirectory, table);
             }
-
+            IEnumerable<FileInfo> subFilesList = directory.EnumerateFiles();
             foreach (FileInfo subFile in subFilesList)
             {
-                GameObject subCube = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                subCube.transform.SetParent(table.transform);
-              
-
-                scale = subCube.transform.localScale;
-                x = scale.x;
-                z = scale.z;
-
-                if ((-SCALE_MULTIPLIER * z) + xInTable * GAP_BETWEEN_ELEMENTS >= EDGE_OF_TABLE)
-                {
-                    xInTable = 0;
-                    yInTable++;
-                }
-
-                x = (-SCALE_MULTIPLIER * x) + yInTable * GAP_BETWEEN_ELEMENTS;
-                z = (-SCALE_MULTIPLIER * z) + xInTable * GAP_BETWEEN_ELEMENTS;
-
-                subCube.transform.localPosition = new Vector3(x, DIRECTORIES_HEIGHT, z);
-                xInTable++;
+                generateSubElement(subFile, table);
             }
+            Debug.Log(tModel.text);
+            tModel.text = "Test";
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
+
