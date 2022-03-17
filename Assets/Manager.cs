831d1f7f80d9ef04aa727835950794c9da5a05d1
Ripoll <rip@ripollTech2.localdomain>
2022-03-17 17:03:39 +0100

    aie

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -1,34 +1,112 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
+using System.IO;
+using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
 
 public class Manager : MonoBehaviour
 {
+    [SerializeField]
     // Start is called before the first frame update
-    void Start()
+
+    public const float GLOBAL_SCALE_MULTIPLIER = 0.2f;
+
+    float SCALE_MULTIPLIER = 3.5f;
+    float DIRECTORIES_HEIGHT = 1.3f * GLOBAL_SCALE_MULTIPLIER;
+    float EDGE_OF_TABLE = 0.4325f;
+    float GAP_BETWEEN_ELEMENTS = 0.2171f;
+    float GAP_BETWEEN_TABLES = 4 * GLOBAL_SCALE_MULTIPLIER;
+    float GAP_HEIGHT_ELEMENT_OVER_TABLES = 1.1f;
+    float i = 0;
+    float sizeOfTable = 8;
+    float x;
+    float z;
+    Vector3 scale;
+    int xInTable;
+    int yInTable;
+    private static DirectoryInfo sourceDirectory = new DirectoryInfo("C:\\Users\\Rip\\Documents\\Code\\SpaceChess\\");
+    IEnumerable<DirectoryInfo> directoryList = sourceDirectory.EnumerateDirectories();
+    void generateSubElement(object typeOfElement, GameObject tableParam)
     {
-        List<string> directoryList = new List<string>(System.IO.Directory.EnumerateDirectories("C:/Users/Rip/Documents/CODE/DigiCom"));
-        int y = 10;
-        int sizeOfTable = 8;
-        foreach (string directory in directoryList)
+
+        GameObject subElement;
+        if (typeOfElement is DirectoryInfo)
         {
-            y+= sizeOfTable;
-            GameObject table = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            table.transform.localScale = new Vector3(sizeOfTable, 1, sizeOfTable);
-            table.transform.position = new Vector3(y, 0, y);
-            Debug.Log(directory.ToString());
-            List<string> subDirectoriesList = new List<string>(System.IO.Directory.EnumerateDirectories(directory.ToString()));
-            foreach (string subDir in subDirectoriesList)
-            {
-                GameObject subCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                subCube.transform.position = new Vector3(y++, 0.5f, y++);
-            }
+            subElement = GameObject.CreatePrimitive(PrimitiveType.Cube);
         }
+        else if (typeOfElement is FileInfo)
+        {
+            subElement = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        }
+        else
+        {
+            return;
+        }
+        subElement.transform.SetParent(tableParam.transform);
+        subElement.transform.localPosition = Vector3.zero;
+
+        subElement.transform.localScale = new Vector3(GLOBAL_SCALE_MULTIPLIER, GLOBAL_SCALE_MULTIPLIER, GLOBAL_SCALE_MULTIPLIER);
+        
+
+        scale = subElement.transform.localScale;
+        x = scale.x;
+        z = scale.z;
+
+        if ((-SCALE_MULTIPLIER * z) + xInTable * GAP_BETWEEN_ELEMENTS >= EDGE_OF_TABLE)
+        {
+            xInTable = 0;
+            yInTable++;
+        }
+
+        x = (-SCALE_MULTIPLIER * x) + yInTable * GAP_BETWEEN_ELEMENTS;
+        z = (-SCALE_MULTIPLIER * z) + xInTable * GAP_BETWEEN_ELEMENTS;
+
+        subElement.transform.localPosition = new Vector3(x, GAP_HEIGHT_ELEMENT_OVER_TABLES, z);
+        xInTable++;
+
+        subElement.AddComponent<XRGrabInteractable>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        foreach (DirectoryInfo directory in directoryList)
+        {
+            GameObject newParent = new GameObject("tableParent");
+            i += (sizeOfTable + GAP_BETWEEN_TABLES);
+            GameObject table = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            newParent.transform.position = new Vector3(0, DIRECTORIES_HEIGHT, i * GLOBAL_SCALE_MULTIPLIER);
+
+            table.transform.SetParent(newParent.transform);
+            table.transform.localPosition = Vector3.zero;
+            table.transform.localScale = new Vector3(sizeOfTable * GLOBAL_SCALE_MULTIPLIER, GLOBAL_SCALE_MULTIPLIER, sizeOfTable * GLOBAL_SCALE_MULTIPLIER);
+
+
+            IEnumerable<DirectoryInfo> subDirectoriesList = directory.EnumerateDirectories();
+
+            xInTable = 0;
+            yInTable = 0;
+
+            foreach (DirectoryInfo subDirectory in subDirectoriesList)
+            {
+                generateSubElement(subDirectory, newParent);
+            }
+            IEnumerable<FileInfo> subFilesList = directory.EnumerateFiles();
+            foreach (FileInfo subFile in subFilesList)
+            {
+                generateSubElement(subFile, newParent);
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
+
